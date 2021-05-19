df81dccf13d064e92365f1bf3c6cf1b6d416b142
worachat.p <worachat.p@ideabranch.co.th>
2021-05-19 14:26:50 +0700

    first Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Hello2, { Hello } from './component/Hello';
+
+function App() {
+  return (
+    <div className="App">
+      <Hello />
+      <Hello2 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Hello.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export function Hello() {
+    return (
+        <div>
+            Hello
+        </div>
+    )
+}
+
+export default function Hello2() {
+    return (
+        <div>
+            Hello2
+        </div>
+    )
+}
